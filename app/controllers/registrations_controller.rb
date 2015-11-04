255f91923f9443991961f3f3b5be2e8eaa3b311e
Brian Dunlap <briandunlap589@gmail.com>
2015-11-04 18:34:16 -0500

    Tweak params in application controller and add jbuilder files to registration views

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,18 @@
 class RegistrationsController < ApplicationController
 def create
-    @user = User.new(email: params[:email],
-                     username: params[:username],
+    @user = User.new(username: params[:username],
+                     full_name: params[:full_name],
+                     email: params[:email],
                      password: params[:password])
     if @user.save
       render "create.json.jbuilder", status: :created
-      # render json: { user: @user }, status: :ok
-        # status: 201
+      # status: 201
     else
       render json: { errors: @user.errors.full_messages },
         status: :unprocessable_entity
         # status: 422
     end
   end
-
 
   def destroy
     @user = User.find_by(email: params[:email])
@@ -22,6 +21,7 @@ def create
     else
       render json: { error: "Invalid email (#{params[:email]}) or password." },
         status: :unauthorized
+        # status: 401
     end
   end
 
@@ -29,10 +29,11 @@ def create
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       render "login.json.jbuilder", status: :ok
-      # render json: { user: @user }, status: :ok
+      # status 200
     else
       render json: { error: "Could not find user for #{params[:username]} or wrong password." },
         status: :unauthorized
+        # status: 401
     end
   end
 end
