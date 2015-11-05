036edd3d606dae9e05ac3ff949c37f8a9c638cc5
Brian Dunlap <briandunlap589@gmail.com>
2015-11-05 15:53:25 -0500

    Debug cards controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -16,7 +16,7 @@ class CardsController < ApplicationController
   def create
     @deck = Deck.find_by(id: params[:id])
     if @deck
-      card = deck.cards.create(question: params[:question],
+      @card = @deck.cards.create(question: params[:question],
                                answer: params[:answer])
       render "create.json.jbuilder"
     else
@@ -28,21 +28,22 @@ class CardsController < ApplicationController
 
   def update
     @deck = Deck.find_by(id: params[:id])
-    if @deck && current_user.id == deck.user_id
+    if @deck && current_user.id == @deck.user_id
       @card = @deck.cards.find_by(id: params[:id])
       if @card
-        @card.update(question: params[:question]
+        @card.update(question: params[:question],
                      answer: params[:answer])
         render "update.json.jbuilder"
       else
-        render json: { error: "Card was not found" }
+        render json: { error: "Card was not found" },
           status: :expectation_failed
             # status 417
       end
     else 
-      render json: { error: "Either the deck does not exist, or it doesn't belong to #{current_user.username}."}
+      render json: { error: "Either the deck does not exist, or it doesn't belong to #{current_user.username}."},
         status: :unauthorized
         # status: 401
+    end
   end
 
   def destroy
@@ -51,17 +52,18 @@ class CardsController < ApplicationController
       @card = @deck.cards.find_by(id: params[:id])
       if @card
         @card.destroy
-        render plain: "The card has been deleted successfully"
+        render plain: "The card has been deleted successfully.",
           status: :accepted
           # status: 202
       else
-        render json: { error: "The card you requested does not exist." }
+        render json: { error: "The card you requested does not exist." },
           status: :not_found
           # status: 404
       end
     else
-      render json: { error: "Either the deck does not exist, or it doesn't belong to #{current_user.username}." }
+      render json: { error: "Either the deck does not exist, or it doesn't belong to #{current_user.username}." },
         status: :unauthorized
         # status: 401
+    end
   end
 end
