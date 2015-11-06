92e77714207632a7d5d542c5843a65cb26a2bd6b
Brian Dunlap <briandunlap589@gmail.com>
2015-11-05 20:30:15 -0500

    Debug cards controller and update README to include relevant documentation for cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,24 +1,24 @@
 class CardsController < ApplicationController
   def index
     @deck = Deck.find_by(id: params[:id])
-    if @deck
-      @deck.cards.each do |card|
-         render "index.json.jbuilder", status: :ok
-          # status 200
-        end
+    @cards = @deck.cards
+    if @cards
+      render "index.json.jbuilder", status: :ok
+        # status 200
     else 
-      render json: { error: "Deck was not found." },
+      render json: { error: "There are no cards to display." },
         status: :not_found
           # status 404
     end
   end
 
   def create
-    @deck = Deck.find_by(id: params[:id])
-    if @deck
+    @deck = Deck.find(params[:id])
+    if @deck && @deck.user_id = current_user.id
       @card = @deck.cards.create(question: params[:question],
                                answer: params[:answer])
-      render "create.json.jbuilder"
+      render "show.json.jbuilder", status: :created
+        # status 201
     else
       render json: { error: "Deck was not found. The new card was not created successfully." },
         status: :expectation_failed
@@ -27,41 +27,34 @@ class CardsController < ApplicationController
   end
 
   def update
-    @deck = Deck.find_by(id: params[:id])
-    if @deck && current_user.id == @deck.user_id
-      @card = @deck.cards.find_by(id: params[:id])
-      if @card
-        @card.update(question: params[:question],
-                     answer: params[:answer])
-        render "update.json.jbuilder"
-      else
-        render json: { error: "Card was not found" },
+    @card = Card.find_by(id: params[:id])
+    deck = current_user.decks.first
+    if @card && @card.deck_id == deck.id
+      @card.update(question: params[:question],
+                   answer: params[:answer])
+        render "show.json.jbuilder", status: :accepted
+          # status 202
+    else
+        render json: { error: "Either the card was not found or it does not belong to #{current_user.username}" },
           status: :expectation_failed
             # status 417
-      end
-    else 
-      render json: { error: "Either the deck does not exist, or it doesn't belong to #{current_user.username}."},
-        status: :unauthorized
-        # status: 401
     end
   end
 
   def destroy
-    @deck = Deck.find_by(id: params[:id])
-    if @deck && current_user.id == @deck.user_id
-      @card = @deck.cards.find_by(id: params[:id])
-      if @card
-        @card.destroy
-        render plain: "The card has been deleted successfully.",
-          status: :accepted
-          # status: 202
-      else
-        render json: { error: "The card you requested does not exist." },
-          status: :not_found
-          # status: 404
-      end
+    @card = Card.find_by(id: params[:id])
+    deck = current_user.decks.first
+    if @card && @card.deck_id == deck.id
+    # @deck = Deck.find(params[:id])
+    # if @deck && current_user.id == @deck.user_id
+    #   @card = @deck.cards.find_by(id: params[:id])
+    #   if @card
+      @card.destroy
+      render plain: "The card has been deleted successfully.",
+        status: :accepted
+        # status: 202
     else
-      render json: { error: "Either the deck does not exist, or it doesn't belong to #{current_user.username}." },
+      render json: { error: "The card you requested does not exist, or you aren't authorized to delete it." },
         status: :unauthorized
         # status: 401
     end
